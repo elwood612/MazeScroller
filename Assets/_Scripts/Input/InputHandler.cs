37d3a7fae05c47e4f583e82dae3d7934940d60be
elwood612 <ray.herrly@gmail.com>
2023-01-20 12:11:10 -0800

    Setting up Input System

--- Assets/_Scripts/Input/InputHandler.cs
+++ Assets/_Scripts/Input/InputHandler.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Controls _controls;
+    private static bool _isPressingScreen;
+    private static Vector2 _pressPosition = new Vector2();
+
+    public static bool IsPressingScreen => _isPressingScreen;
+    public static Vector2 PressPosition => _pressPosition;
+
+    
+
+    private void Awake()
     {
-        
+        _controls = new Controls();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
+        _controls.Enable();
         
+        _controls.Gameplay.Press.performed += HandlePress;
+        _controls.Gameplay.Press.canceled += HandlePress;
+        _controls.Gameplay.Draw.performed += HandleDraw;
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+        
+        _controls.Gameplay.Press.performed -= HandlePress;
+        _controls.Gameplay.Press.canceled -= HandlePress;
+        _controls.Gameplay.Draw.performed -= HandleDraw;
+    }
+
+    private void HandleDraw(InputAction.CallbackContext ctx)
+    {
+        _pressPosition = ctx.ReadValue<Vector2>();
+    }
+
+    private void HandlePress(InputAction.CallbackContext ctx)
+    {
+        _isPressingScreen = ctx.ReadValueAsButton();
+    }
+
+    private void Update()
+    {
+        if (IsPressingScreen) Debug.Log("Position = " + PressPosition);
     }
 }
